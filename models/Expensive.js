ebe1dc2616ceb5db00f209afc84377ccf5ec90bd
DilshanDilipudara <gunawardhanaudara@gmail.com>
2020-02-10 23:43:06 +0530

    add Expensive model

---
+++ models/Expensive.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');
+
+var ExpensiveSchema = new Schema({
+ 
+    expensiveID:{
+        type:Number,
+        required:true
+    },
+    Expensive_type:String,
+    amount:Number,
+    Date:Date,
+    UserID:Number
+    
+});
+
+
+module.exports = mongoose.model('User', UserSchema);
